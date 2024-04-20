56b8e60d01533c80564ee042128583807b7113c6
Dan Moffat <danmoffat@gmail.com>
2024-04-20 23:31:30 +0100

    Added types for children

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import CourseGoal from './components/CourseGoal.tsx';
 const App = () => {
   return (
     <main className='flex p-4 space-x-4'>
-      <CourseGoal title='Title' description='Description' />
+      <CourseGoal title='Title' description='Description'>
+        <CourseGoal
+          title='Child component'
+          description="Oooh I'm not a string!"
+        />
+      </CourseGoal>
     </main>
   );
 };
--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,14 +1,18 @@
+import { ReactElement } from 'react';
+
 interface CourseGoalProps {
   title: string;
   description: string;
+  children?: string | ReactElement;
 }
 
-const CourseGoal = ({ title, description }: CourseGoalProps) => {
+const CourseGoal = ({ title, description, children }: CourseGoalProps) => {
   return (
     <article className='bg-slate-200 rounded shadow-md p-4 basis-1/3'>
       <div className='text-slate-900'>
         <h2>{title}</h2>
         <p>{description}</p>
+        <p>{children}</p>
       </div>
       <button className='bg-red-700 text-white rounded py-2 px-5 w-full mt-4 pointer hover:bg-red-600'>
         DELETE
