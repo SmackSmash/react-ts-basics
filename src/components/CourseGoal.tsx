c3ddc34f55cbd4ecda97adaa7f5df23e86e7482e
Dan Moffat <danmoffat@gmail.com>
2024-04-20 21:37:33 +0100

    A little styling

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,11 +1,13 @@
 const CourseGoal = () => {
   return (
-    <article>
-      <div>
+    <article className='bg-slate-200 rounded shadow-md m-4 p-4 w-1/3'>
+      <div className='text-slate-900'>
         <h2>TITLE</h2>
         <p>DESCRIPTION</p>
       </div>
-      <button>DELETE</button>
+      <button className='bg-red-700 text-white rounded py-2 px-5 w-full mt-4 pointer hover:bg-red-600'>
+        DELETE
+      </button>
     </article>
   );
 };
