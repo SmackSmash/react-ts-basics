4b018cbe4ac8e9b83b81d50d07b4f5f67373107d
Dan Moffat <danmoffat@gmail.com>
2024-04-21 22:10:58 +0100

    A little refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,10 @@ type HeaderProps = PropsWithChildren<{
   };
 }>;
 
-const Header = ({ image: { src, alt }, children }: HeaderProps) => {
+const Header = ({ image, children }: HeaderProps) => {
   return (
     <header className='w-full h-24 flex justify-between items-center px-4 shadow-md bg-slate-200'>
-      <img className='max-h-full py-4' src={src} alt={alt} />
+      <img className='max-h-full py-4' {...image} />
       {children}
     </header>
   );
