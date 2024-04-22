a96cff41c27c1e3e0857cfd220543e19ed219168
Dan Moffat <danmoffat@gmail.com>
2024-04-23 00:43:55 +0100

    Semantic HTML

--- src/App.tsx
+++ src/App.tsx
@@ -47,13 +47,15 @@ const App = () => {
       <Header image={{ src: HeaderImage, alt: 'A list of goals' }}>
         <h1 className='text-slate-900 text-2xl font-bold'>Your Course Goals</h1>
       </Header>
-      <AddGoalForm
-        title={title}
-        description={description}
-        onTitleChange={handleTitleChange}
-        onDescriptionChange={handleDescriptionChange}
-        onAddGoal={handleAddGoal}
-      />
+      <section>
+        <AddGoalForm
+          title={title}
+          description={description}
+          onTitleChange={handleTitleChange}
+          onDescriptionChange={handleDescriptionChange}
+          onAddGoal={handleAddGoal}
+        />
+      </section>
       <main className='flex p-4 space-x-4'>
         <CourseGoalList goals={goals} onDelete={handleDeleteGoal} />
       </main>
