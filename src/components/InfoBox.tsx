3b736f026a89faa357598a9f43c07bbe0636cb9c
Dan Moffat <danmoffat@gmail.com>
2024-04-23 16:08:03 +0100

    A little refactoring

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -2,23 +2,28 @@ import { type PropsWithChildren } from 'react';
 
 interface InfoBoxProps {
   mode: 'hint' | 'warning';
+  severity?: 'low' | 'medium' | 'high';
 }
 
 const InfoBox = ({ mode, children }: PropsWithChildren<InfoBoxProps>) => {
-  const infoBoxClasses =
+  let infoBoxClasses =
     'border flex flex-col h-20 m-4 items-center justify-center';
 
   if (mode === 'hint') {
+    infoBoxClasses += ' order-cyan-500 text-cyan-500';
+
     return (
-      <aside className={`${infoBoxClasses} border-cyan-500 text-cyan-500`}>
+      <aside className={infoBoxClasses}>
         <p>{children}</p>
       </aside>
     );
   }
 
   if (mode === 'warning') {
+    infoBoxClasses += ' border-orange-500 text-orange-500';
+
     return (
-      <aside className={`${infoBoxClasses} border-orange-500 text-orange-500`}>
+      <aside className={infoBoxClasses}>
         <h2 className='font-bold text-2xl uppercase'>Warning</h2>
         <p>{children}</p>
       </aside>
