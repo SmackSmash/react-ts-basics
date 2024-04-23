cca5d60751aa4bff22fdbb23da059e029c06d310
Dan Moffat <danmoffat@gmail.com>
2024-04-23 01:45:31 +0100

    A little cleanup of reusable classes

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -5,16 +5,19 @@ interface InfoBoxProps {
 }
 
 const InfoBox = ({ mode, children }: PropsWithChildren<InfoBoxProps>) => {
+  const sharedClasses = 'border flex h-20 m-4 items-center justify-center';
+
   if (mode === 'hint') {
     return (
-      <aside className='border border-blue-500 text-blue-500 flex h-20 m-4 items-center justify-center'>
+      <aside className={`${sharedClasses} border-blue-500 text-blue-500`}>
         <p>{children}</p>
       </aside>
     );
   }
+
   if (mode === 'warning') {
     return (
-      <aside className='border border-orange-500 text-orange-500 flex h-20 m-4 items-center justify-center'>
+      <aside className={`${sharedClasses} border-orange-500 text-orange-500`}>
         <p>{children}</p>
       </aside>
     );
